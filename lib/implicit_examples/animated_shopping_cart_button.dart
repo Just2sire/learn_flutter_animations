f204c9712c07608693363fdf41815584335da513
Just2sire <le05onze2004@gmail.com>
2024-02-27 12:21:27 +0000

    Shopping card animation

--- lib/implicit_examples/animated_shopping_cart_button.dart
+++ lib/implicit_examples/animated_shopping_cart_button.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class ShoppingCartButton extends StatelessWidget {
+class ShoppingCartButton extends StatefulWidget {
   const ShoppingCartButton({super.key});
+
+  @override
+  State<ShoppingCartButton> createState() => _ShoppingCartButtonState();
+}
+
+class _ShoppingCartButtonState extends State<ShoppingCartButton> {
+  bool isPressed = false;
 
   @override
   Widget build(BuildContext context) {
@@ -10,16 +17,41 @@ class ShoppingCartButton extends StatelessWidget {
         title: const Text('Shopping Cart'),
       ),
       body: Center(
-        child: Container(
-          width: 80.0,
-          height: 60.0,
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(10.0),
-          ),
-          child: const Icon(
-            Icons.shopping_cart,
-            color: Colors.white,
+        child: GestureDetector(
+          onTap: () => setState(() {
+            isPressed = !isPressed;
+          }),
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 1000),
+            width: isPressed ? 200 : 80.0,
+            height: 60.0,
+            decoration: BoxDecoration(
+              color: isPressed ? Colors.green : Colors.blue,
+              borderRadius: isPressed ? BorderRadius.circular(30) : BorderRadius.circular(10.0),
+            ),
+            child: isPressed
+                ? const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(
+                        Icons.check,
+                        size: 24,
+                        color: Colors.white,
+                      ),
+                      Text(
+                        "Added To Card",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  )
+                : const Icon(
+                    Icons.shopping_cart,
+                    color: Colors.white,
+                  ),
           ),
         ),
       ),
