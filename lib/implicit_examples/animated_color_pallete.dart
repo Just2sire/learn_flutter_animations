4edbdf40d30b2733411886f96e457570d775f33d
Just2sire <le05onze2004@gmail.com>
2024-02-27 11:58:43 +0000

    animatedContainer used

--- lib/implicit_examples/animated_color_pallete.dart
+++ lib/implicit_examples/animated_color_pallete.dart
@@ -41,11 +41,13 @@ class _AnimatedColorPaletteState extends State<AnimatedColorPalette> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             for (Color color in currentPalette)
-              Container(
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 2000),
                 width: 100,
                 height: 100,
                 color: color,
                 margin: const EdgeInsets.all(8),
+                curve: Curves.ease,
               ),
             ElevatedButton(
               onPressed: regeneratePalette,
