b2587b03fe8659b5278c25885a8ed4837c00cb39
Just2sire <le05onze2004@gmail.com>
2024-02-27 16:31:01 +0000

    TweenAnimation

--- lib/implicit_examples/animated_tween_animation_builder_example.dart
+++ lib/implicit_examples/animated_tween_animation_builder_example.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class PulsatingCircleAnimation extends StatelessWidget {
   const PulsatingCircleAnimation({super.key});
-  final double size = 200;
+
+  // final double size = 200;
 
   @override
   Widget build(BuildContext context) {
@@ -11,19 +12,34 @@ class PulsatingCircleAnimation extends StatelessWidget {
         title: const Text('Pulsating Circle Animation'),
       ),
       body: Center(
-        child: Container(
-          width: size,
-          height: size,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.blue,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.blue.withOpacity(0.5),
-                blurRadius: size,
-                spreadRadius: size / 2,
+        child: TweenAnimationBuilder(
+          // La propriété child de TweenAnimationBuilder sert à passer des enfants qu'on ne veut pas rebuild
+          // chaque fois que l'animation est faite. C'est lui le paramètre widget dans le callback du builder
+          duration: const Duration(milliseconds: 1500),
+          tween: Tween<double>(begin: 0, end: 200),
+          builder: (context, size, widget) {
+            return Container(
+              width: size,
+              height: size,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blue,
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: Colors.blue.withOpacity(0.5),
+                    blurRadius: size,
+                    spreadRadius: size / 2,
+                  ),
+                ],
               ),
-            ],
+              // child: widget,
+            );
+          },
+          child: const AnimatedScale(
+            duration: Duration(milliseconds: 1500),
+            scale: 1.5,
+            child: Text("Hello World!"),
           ),
         ),
       ),
